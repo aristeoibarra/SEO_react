a8ebe18c50b3211e4349f94e2a68bfd93df01f9d
aristeoibarra <aristeoibarra608@gmail.com>
2022-06-18 06:59:41 -0600

    deploy7

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,27 @@
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Posts from "./Pages/Posts";
+import { hydrate, render } from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="posts" element={<Posts />} />
-    </Routes>
-  </BrowserRouter>
-);
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="posts" element={<Posts />} />
+      </Routes>
+    </BrowserRouter>,
+    rootElement
+  );
+} else {
+  render(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="posts" element={<Posts />} />
+      </Routes>
+    </BrowserRouter>,
+    rootElement
+  );
+}
