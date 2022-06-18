d67e0e263f86c0be2fe4881685f4ca480d6f9787
aristeoibarra <aristeoibarra608@gmail.com>
2022-06-18 07:25:04 -0600

    deploy8

--- src/Pages/Posts.jsx
+++ src/Pages/Posts.jsx
@@ -15,7 +15,31 @@ export default function Posts() {
           content="Posts - description"
         ></meta>
         <meta name="author" content="React" />
+        <meta name="image" content="https://seo.telloim.com/react.png" />
         <link rel="canonical" href="https://seo.telloim.com" />
+
+        <meta property="og:type" content="website"></meta>
+        <meta property="og:url" content="https://seo.telloim.com/posts/"></meta>
+        <meta property="og:title" content="Posts - React"></meta>
+        <meta property="og:description" content="Posts - description"></meta>
+        <meta
+          property="og:image"
+          content="https://seo.telloim.com/react.png"
+        ></meta>
+
+        <meta property="twitter:card" content="this is a twitter card"></meta>
+        <meta
+          property="twitter:url"
+          content="https://seo.telloim.com/posts/"
+        ></meta>
+        <meta
+          property="twitter:description"
+          content="Posts - description"
+        ></meta>
+        <meta
+          property="twitter:image"
+          content="https://seo.telloim.com/react.png"
+        ></meta>
       </Helmet>
 
       <h1>Posts</h1>
