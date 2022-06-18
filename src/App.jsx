5d2dd36f44a8a416b8a3ebe8b0d5ce22aaa66644
aristeoibarra <aristeoibarra608@gmail.com>
2022-06-18 06:54:51 -0600

    deploy6

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 export default function App() {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Home</title>
+      </Helmet>
       <h1>App-test</h1>
       <nav
         style={{
--- src/Pages/Posts.jsx
+++ src/Pages/Posts.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 
-import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Helmet } from "react-helmet";
 
 export default function Posts() {
   return (
     <div>
-      <HelmetProvider>
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>Posts - React</title>
-          <meta name="title" property="og:title" content="Posts - React"></meta>
-          <meta
-            name="description"
-            property="og:description"
-            content="Posts - description"
-          ></meta>
-          <meta name="author" content="React" />
-          <link rel="canonical" href="https://seo.telloim.com" />
-        </Helmet>
-      </HelmetProvider>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Posts - React</title>
+        <meta name="title" property="og:title" content="Posts - React"></meta>
+        <meta
+          name="description"
+          property="og:description"
+          content="Posts - description"
+        ></meta>
+        <meta name="author" content="React" />
+        <link rel="canonical" href="https://seo.telloim.com" />
+      </Helmet>
 
       <h1>Posts</h1>
     </div>
