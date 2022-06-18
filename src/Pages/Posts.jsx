3301fd6391f1df95f3cd9d29da12783b97c8dd53
aristeoibarra <aristeoibarra608@gmail.com>
2022-06-18 06:07:25 -0600

    deploy4

--- src/Pages/Posts.jsx
+++ src/Pages/Posts.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 export default function Posts() {
-  return <div>Posts</div>;
+  return (
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Posts - React</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+
+      <h1>Posts</h1>
+    </div>
+  );
 }
